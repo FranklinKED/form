6cc784ecc328ef77852e91c3c744310e32d3fdc7
Franklin Ezemmouka <kedgraphix@gmail.com>
2023-03-07 21:21:50 -0800

    Created a form using html, css and Javascript

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+const btn = document.getElementById("button");
+btn.addEventListener("click", () =>{
+ alert("Form Submitted Successfully!👍")
+});
+
+
+// const form = document.getElementById("form");
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     validateForm();
+//   });
+
+
+//  const form = document.getElementById("myForm");
+//  form.addEventListener("submit", function(validateForm))
+//  validateForm.preventDefault();
+
+// function validateForm() {
+//     // Get the form fields values
+//     var name = document.forms["myForm"]["name"].value;
+//     var email = document.forms["myForm"]["email"].value;
+//     var message = document.forms["myForm"]["message"].value;
+//     var password = document.forms["myForm"]["password".value];
+
+//     // Check if all fields have been filled
+//     if (name == "" || email == "" || message == "" || password == "") {
+//         alert("Please fill out all fields😢.");
+//         return false;
+//     } else {
+//         alert("Success! Form submitted👍.");
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
